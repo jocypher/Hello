5cc7cba5a820cdc5ea2883ce527fdfb4178465f2
Jonathan Wilchield Arthur(10945544) <100198275+jocypher@users.noreply.github.com>
2022-06-17 11:47:58 +0000

    Update password.py

--- password.py
+++ password.py
@@ -2,3 +2,8 @@ import random
 upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 lower_case = "abcdefghijklmnopqrstuvwxyz"
 numbers = "0123456789"
+symbols = "!@#$%&*.?,:;][}{"
+user_password = upper_case + lower_case + numbers + symbols 
+user_length = 8
+password = "".join(random.sample(user_password, user_length))
+print("Your Password is", password)
